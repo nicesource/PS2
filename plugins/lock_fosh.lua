e7853a8dc5b7ecfe3352e46aad850ee3b8ac15e2
nicesource <sourceeme@gmail.com>
2016-10-24 23:38:51 +0330

    Create lock_fosh.lua

---
+++ plugins/lock_fosh.lua
@@ -0,0 +1,32 @@
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_fosh'] then
+                lock_fosh = data[tostring(msg.to.id)]['settings']['lock_fosh']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_fosh == "yes" then
+       delete_msg(msg.id, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+    "nnt",
+    "nanat",
+    "kos",
+    "kir",
+    "کیر",
+    "کص",
+    "ننت",
+    "حرومزاده",
+  },
+  run = run
+}
