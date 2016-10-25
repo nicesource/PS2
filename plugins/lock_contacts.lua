9fc6f3447c07192a17809405ee65fce1fba55552
nicesource <sourceeme@gmail.com>
2016-10-25 18:12:31 +0330

    Update lock_contacts.lua

--- plugins/lock_contacts.lua
+++ plugins/lock_contacts.lua
@@ -5,14 +5,14 @@ local function run(msg, matches)
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['number'] then
-                number = data[tostring(msg.to.id)]['settings']['number']
+            if data[tostring(msg.to.id)]['settings']['lock_contacts'] then
+                lock_contacts = data[tostring(msg.to.id)]['settings']['lock_contacts']
             end
         end
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if number == "yes" then
+    if lock_contacts == "yes" then
        delete_msg(msg.id, ok_cb, true)
     end
 end
